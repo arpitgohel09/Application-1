a4de1b6e19092082927a0dc3331831d39e351982
unknown <arpitgohil@LAPTOP-8A8EDS4O.com>
2021-01-13 11:47:56 +0530

    First commit

---
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void send(View view){
+        Toast.makeText(this, "Sending message from device......", Toast.LENGTH_SHORT).show();
+    }
+    public void Receive(View view){
+        Toast.makeText(this, "Receiving message......", Toast.LENGTH_SHORT).show();
+    }
+    public void Delete(View view){
+        Toast.makeText(this, "Deleting message from device......", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
